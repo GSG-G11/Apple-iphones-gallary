97253b96b506c5e87bcf5c98a3a2081fbebb3154
Amran <amram.man99@gmail.com>
2022-03-02 16:02:29 +0200

    finish of handle css relates #9

--- public/js/dom.js
+++ public/js/dom.js
@@ -15,27 +15,32 @@ inputSearch.addEventListener('submit', (e)=>{
         image.src = data.data.phone_images[0]
         container.appendChild(image)
         inputValue.value = '';
-
+        
         const name = document.createElement('span')
         name.innerText = data.data.phone_name;
+        // name.style.backgroundColor = 'black'
         container.appendChild(name)
         
 
         
         const os = document.createElement('span')
         os.innerText = data.data.os;
+        // os.style.backgroundColor= 'black'
         container.appendChild(os)
         
         const dimension = document.createElement('span')
         dimension.innerText = data.data.dimension;
+        // dimension.style.backgroundColor = 'black'
         container.appendChild(dimension)
 
         const release_date = document.createElement('span')
         release_date.innerText = data.data.release_date;
+        // release_date.style.backgroundColor = 'black'
         container.appendChild(release_date)
 
         const storage = document.createElement('span')
         storage.innerText = data.data.storage;
+        // storage.style.backgroundColor = 'black'
         container.appendChild(storage)
 
     })
