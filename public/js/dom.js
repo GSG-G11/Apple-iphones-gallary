9609fde01ab1107e7def8000c84b719641d7e12d
MostafaQanoo <mostafaqanoo403@gmail.com>
2022-03-02 17:28:55 +0200

    handlle error and create readme

--- public/js/dom.js
+++ public/js/dom.js
@@ -3,47 +3,44 @@ const container = document.querySelector('.contentImg')
 const inputValue = document.querySelector('#input')
 
 
-
 inputSearch.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    container.textContent = '';
-    const search = e.target.querySelector('input').value;
-    fetch(`/phone?search=${search}`)
-    .then(data => data.json())
-    .then(data=> { 
-        const image = document.createElement('img')
-        image.src = data.data.phone_images[0]
-        container.appendChild(image)
-        inputValue.value = '';
-        
-        const name = document.createElement('span')
-        name.innerText = data.data.phone_name;
-        // name.style.backgroundColor = 'black'
-        container.appendChild(name)
-        
+e.preventDefault();
+container.textContent = '';
+const search = e.target.querySelector('input').value;
+fetch(`/phone?search=${search}`)
+.then(data => data.json())
+.then(data=> {
+     if(data.message){
+        const result = document.createElement('span')
+        result.innerText = data.message;
+        container.appendChild(result)
+     }else{
+const image = document.createElement('img')
+image.src = data.data.phone_images[0]
+container.appendChild(image)
+inputValue.value = '';
 
-        
-        const os = document.createElement('span')
-        os.innerText = data.data.os;
-        // os.style.backgroundColor= 'black'
-        container.appendChild(os)
-        
-        const dimension = document.createElement('span')
-        dimension.innerText = data.data.dimension;
-        // dimension.style.backgroundColor = 'black'
-        container.appendChild(dimension)
+const name = document.createElement('span')
+name.innerText = data.data.phone_name;
+container.appendChild(name)
 
-        const release_date = document.createElement('span')
-        release_date.innerText = data.data.release_date;
-        // release_date.style.backgroundColor = 'black'
-        container.appendChild(release_date)
+const os = document.createElement('span')
+os.innerText = data.data.os;
+container.appendChild(os)
+const dimension = document.createElement('span')
+dimension.innerText = data.data.dimension;
+container.appendChild(dimension)
 
-        const storage = document.createElement('span')
-        storage.innerText = data.data.storage;
-        // storage.style.backgroundColor = 'black'
-        container.appendChild(storage)
+const release_date = document.createElement('span')
+release_date.innerText = data.data.release_date;
+container.appendChild(release_date)
 
-    })
+const storage = document.createElement('span')
+storage.innerText = data.data.storage;
+container.appendChild(storage)
+     }
 
 
+})
+.catch(error => console.log(error))
 })
--- src/app.js
+++ src/app.js
@@ -1,40 +1,28 @@
-
+const fetch = require('node-fetch');
+const { status } = require('express/lib/response');
 const express = require('express');
 const path = require('path')
 const app = express();
-const fetch = require('node-fetch');
-const { status } = require('express/lib/response');
+
 
 app.disable('x-powered-by')
-
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
-
 app.get('/phone', (req, res)=>{
-    fetch(`http://api-mobilespecs.azharimm.site/v2/search?query=${req.query.search}`)
-    .then(data=> data.json())
-    .then (data => fetch(data.data.phones[0].detail))   
-    .then(data=> data.json())
-    .then(data => {
-        res.json(data)
+fetch(`http://api-mobilespecs.azharimm.site/v2/search?query=${req.query.search}`)
+.then(data=> data.json())
 
-    })
-    
+.then (data => fetch(data.data.phones[0].detail)) 
+.then(data=> data.json())
+.then(data => {res.json(data)})
+.catch(error => {res.status(404).json({
+message:'result not found'
 })
-
-
-
-
-
-
-
-
-const port = process.env.PORT||8000;
-app.listen(port, ()=>{
-    console.log(`the server is runnig in  ${port}`);
+;})
 })
+module.exports = app ;
+
